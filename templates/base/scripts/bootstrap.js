6410cc187dea5abd842082fe6d4ef1de8c92ecfd
Adrien Hobbs <adrien.hobbs@gmail.com>
2019-12-22 09:41:41 -0500

    import from shared

--- templates/base/scripts/bootstrap.js
+++ templates/base/scripts/bootstrap.js
@@ -1,10 +1,1 @@
-import "../styles/index.scss";
-
-import {
-  defineCustomElements,
-  applyPolyfills
-} from "@weareglow/media-components/loader";
-
-applyPolyfills().then(() => {
-  defineCustomElements(window);
-});
+import shared from "@sharedScripts";
